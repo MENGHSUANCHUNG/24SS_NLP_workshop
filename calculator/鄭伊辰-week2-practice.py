1180d283846e381c7b39759ff8f4d76a4c724b32
VKiskra <155826256+VKiskra@users.noreply.github.com>
2024-03-13 15:10:39 +0800

    Update 鄭伊辰-week2-practice.py

--- calculator/鄭伊辰-week2-practice.py
+++ calculator/鄭伊辰-week2-practice.py
@@ -1,7 +1,7 @@
 print("請依序輸入數字 運算符號 數字")
-n1 = int(input("enter n1:"))
-oprator = ord(input("operator:"))
-n2 = int(input("enter n2:"))
+n1 = int(input("Enter n1:"))
+oprator = ord(input("Enter operator:"))
+n2 = int(input("Enter n2:"))
 
 if(oprator == 43):
     print(n1 + n2)
