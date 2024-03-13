c8830a6ff1eccae05a253dc8d50d1bde0100af2d
weichengwillie <weicheng911011@gmail.com>
2024-03-13 15:28:21 +0800

    彭偉誠.py

--- calculator/彭偉誠.py
+++ calculator/彭偉誠.py
@@ -3,14 +3,15 @@ n2 = int(input("輸入數字B:"))
 op = input("輸入一個四則運算符號(加+,減-,乘*,除/,整除//):")
 
 if op == "+" :
-    print("A+B = %s" % (n1+n2))
+    print(n1+n2)
 elif op == "-":
-    print("A-B = %s" % (n1-n2))
+    print(n1-n2)
 elif op == "*":
-    print("A*B = %s" % (n1*n2))
+    print(n1*n2)
 elif op == "/": 
-    print("A/B = %s" % (n1/n2))
+    print(n1/n2)
 elif op == "//":
-    print("A//B = %s" % (n1//n2))
+    print(n1//n2)
+    
 else:
     print("符號輸入錯誤")
