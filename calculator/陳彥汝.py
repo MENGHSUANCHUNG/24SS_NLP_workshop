d97788d49ee08345f4adeceafe4a96630b340524
LisaLeticia <160850149+LisaLeticia@users.noreply.github.com>
2024-03-13 15:10:14 +0800

    陳彥汝.py

---
+++ calculator/陳彥汝.py
@@ -0,0 +1,13 @@
+n1 = int(input("請輸入數字一: "))
+x = input("請輸入運算符號: ")
+n2 = int(input("請輸入數字二: "))
+if x == "+":
+    print(n1+n2)
+elif x == "-":
+    print(n1-n2)
+elif x == "*":
+    print(n1*n2)
+elif x == "/":
+    print(n1/n2)
+else:
+    print("請重新輸入")
