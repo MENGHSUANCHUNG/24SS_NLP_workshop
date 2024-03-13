d4281ae48f8916437aa00916f46899c20c20517c
VKiskra <155826256+VKiskra@users.noreply.github.com>
2024-03-13 15:21:51 +0800

    Update 鄭伊辰-week2-practice.py

--- calculator/鄭伊辰-week2-practice.py
+++ calculator/鄭伊辰-week2-practice.py
@@ -1,15 +1,15 @@
 print("請依序輸入數字 運算符號 數字")
 n1 = int(input("Enter n1:"))
-oprator = ord(input("Enter operator:"))
+oprator = input("Enter operator:")
 n2 = int(input("Enter n2:"))
 
-if(oprator == 43):
+if(oprator == "+"):
     print(n1 + n2)
-elif(oprator == 45):
+elif(oprator == "-"):
     print(n1 - n2)
-elif(oprator == 42):
+elif(oprator == "*"):
     print(n1 * n2)
-elif(oprator == 47):
+elif(oprator == "/"):
     if(n2 == 0):
         print("除數不能為 0")
     else:
