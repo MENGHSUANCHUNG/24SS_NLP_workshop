40b1f63d838539f4f1804fce55668627e2cce5fd
ChenJonathan-tw <chenjonathan901210@gmail.com>
2024-03-19 16:56:08 +0800

    Week03 Prep

---
+++ Week03_Case_Extraction/Week_03_Basic.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# Variable/ Assign
+#a = 4
+#b = 2
+
+# Operators
+# + - * / // ** %
+#print(a + b)
+#print(a - b)
+#print(a * b)
+#print(a / b)
+#print(a // b)
+#print(a % b)
+#print(a ** b)
+
+# ==
+#print(a == b)
+#print(a == b*2)
+
+# Data Types
+#print(type(1))
+#print(type(1.0))
+#print(type("1"))
+#print(type(True))
+
+# Conditionals
+# if else
+
+# List Tuple
+#intLIST = [1,2,3,4,5] 
+#intTUPLE = (1,2,3,4,5)
+#print(intLIST[::2])
+#intLIST[0] = 0
+#print(intLIST)
+#intTUPLE[0] = 0
+# len()
+
+# Dictionary
+# vocabDICT = {"apple":12, "banana":13}
+# vocabDICT["apple"] = 10
+
+# function and program
+# f(x) = 2x+1
+
+#def f(x):
+    #'''
+    #input:
+    #x    =>   Int: Random Positive Integer
+    #output:
+    #y    =>   Int: Even Positive Integer
+    #'''
+    #if x % 2 == 1:
+        #y = x*2
+    #else:
+        #y = x
+        
+    #print(f"f({x}) = {y}")
+    
+#if __name__ == "__main__":
+    #x = 2
+    #f(x)
+    
+
+# for loop
